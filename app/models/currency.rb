8c2498750114856b6b4859bbb48dcd9af753b2c4
Benjamin Steidl <localartpool@gmail.com>
2021-01-18 13:24:10 +0100

    Worked on api call

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,10 @@
 class Currency < ApplicationRecord
+    
+  apikey = ENV.fetch('COIN_KEY')
+
+  def current_price
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key
+    request = HTTParty.get(url + self.slug)
+    response = JSON.parse(request.body)
+  end 
 end
