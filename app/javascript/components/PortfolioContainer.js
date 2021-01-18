c68dd3e3c40581fca6acb2fabe3c213cf8d9a7f3
Benjamin Steidl <localartpool@gmail.com>
2021-01-18 16:11:16 +0100

    Worked on search component

--- app/javascript/components/PortfolioContainer.js
+++ app/javascript/components/PortfolioContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Calculate from './Calculate'
 import Search from './Search'
-
+import axios from 'axios' 
 
 class PortfolioContainer extends Component {
   constructor(props) {
@@ -14,12 +14,31 @@ class PortfolioContainer extends Component {
       active_currency: null,
       amount: '' 
     }
+    
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+    axios.post('https://localhost:3000/search', {
+      name: this.state.name
+    })
+    .then((data) => {
+      debugger
+    })
+    .catch( () => {
+      debugger
+    })
+    
+    console.log(this.state.name)
   }
 
   render(){
     return(
       <div>
-        <Search />
+        <Search handleChange={this.handleChange}/>
         <Calculate />
       </div>
     )
--- app/javascript/components/Search.js
+++ app/javascript/components/Search.js
@@ -7,7 +7,20 @@ class Search extends Component {
   render() {
     return(
       <div>
-        Search goes her
+        <h1>Cryptocurrency Portfolio Caalculator</h1>
+        <form action="">
+          <div className="form-group" >
+            <label>Search for currency: </label><br/>
+            <input 
+              onChange={this.props.handleChange}
+              autoComplete="off" 
+              type="text" 
+              name="name" 
+              placeholder="Ex: Bitcoin, Lightcoin, Ethereum... " 
+              value={this.props.name} 
+              className="filed"/>
+          </div>
+        </form>
       </div>
     )
   }
