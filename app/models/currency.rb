11644672e5294c0b05f5555d2a473bfc51c4b5bb
Benjamin Steidl <localartpool@gmail.com>
2021-01-18 15:01:49 +0100

    Worked on currencies controller

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,10 +1,14 @@
 class Currency < ApplicationRecord
+
+  def calculate_value(amount)
+    (current_price.to_f * amount.to_f).round(4)
+  end
     
   apikey = ENV.fetch('COIN_KEY')
 
   def current_price
-    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey.key
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apikey 
     request = HTTParty.get(url + self.slug)
-    response = JSON.parse(request.body)
+    response = JSON.parse(request.body)[0]['price']
   end 
 end
